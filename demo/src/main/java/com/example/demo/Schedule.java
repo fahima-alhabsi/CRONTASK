4fd7e94a0356b6a07722a83ff3d7282421c6763c
U-Usaid <osaid.osaid66@gmail.com>
2023-05-31 12:01:20 +0400

    question 2 and 3 in cron

--- demo/src/main/java/com/example/demo/Schedule.java
+++ demo/src/main/java/com/example/demo/Schedule.java
@@ -5,7 +5,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Schedule {
-    @Scheduled(cron = "0 */5 * * * *")
+   // ( problem 1)
+//    @Scheduled(cron = "0 */5 * * * *")
+//    public void schedualing(){
+//        System.out.println("Hello fahima");
+//    }
+
+//    (problem 2)
+//    @Scheduled(cron = "0 0 * * * *")
+//public void schedualing(){
+//    System.out.println("Hello fahima");
+//}
+
+ //(problem 3)
+    @Scheduled(cron = "0 30 14 * * *")
     public void schedualing(){
         System.out.println("Hello fahima");
     }
