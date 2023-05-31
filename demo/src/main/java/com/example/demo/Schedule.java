1dda26fbd8eff5a24af6dfd9e84e687d308d04d2
U-Usaid <osaid.osaid66@gmail.com>
2023-05-31 11:16:10 +0400

    first commit

---
+++ demo/src/main/java/com/example/demo/Schedule.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Schedule {
+    @Scheduled(cron = "0 * * * * *")
+    public void schedualing(){
+        System.out.println("Hello fahima");
+    }
+}
