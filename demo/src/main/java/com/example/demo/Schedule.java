1ea36e87e53ab4f3fec6ce6b58d1cdd2df7f7608
U-Usaid <osaid.osaid66@gmail.com>
2023-05-31 11:19:33 +0400

    first question of cron

--- demo/src/main/java/com/example/demo/Schedule.java
+++ demo/src/main/java/com/example/demo/Schedule.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Schedule {
-    @Scheduled(cron = "0 * * * * *")
+    @Scheduled(cron = "0 */5 * * * *")
     public void schedualing(){
         System.out.println("Hello fahima");
     }
